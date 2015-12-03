49576d9c735bd4731af9f6caec428e24397e842d
Skyler Parr <sparr@avvo.com>
2015-12-03 11:07:03 -0800

    Initial Commit

---
+++ WhiteElephantIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhiteElephantIOS
+//
+//  Created by Skyler Parr on 12/3/15.
+//  Copyright © 2015 Skyler Parr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
