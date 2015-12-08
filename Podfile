7631e50bf1d5b64db13cc1c9f449276279c9bfd5
Skyler Parr <sparr@avvo.com>
2015-12-08 12:28:49 -0800

    [wip] initial commit

---
+++ Podfile
@@ -0,0 +1,3 @@
+use_frameworks!
+pod "SwiftPhoenixClient"
+pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
--- WhiteElephantIOS/ViewController.swift
+++ WhiteElephantIOS/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import SwiftPhoenixClient
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var roomIdTextField: UITextField!
+    @IBOutlet weak var connectButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func connectTapped(sender: AnyObject) {
+        let socket = Phoenix.Socket(domainAndPort: "localhost:4000", path: "socket", transport: "websocket")
+        
+        let topic: String = "games:\(roomIdTextField.text!)"
+        
+        NSLog(topic)
+        
+        socket.join(topic: topic, message: Phoenix.Message(subject: "status", body: "joining"), callback: { channel in
+            let chan = channel as! Phoenix.Channel
+            chan.on("phx_reply") { message in
+                NSLog("joined")
+            }
+            chan.on("item_updated") { message in
+                let msg = message as! Phoenix.Message
+                self.itemUpdated(msg.message!)
+            }
+        })
+    }
+    
+    func itemUpdated(message: AnyObject) {
+//        let json = SwiftyJSON.JSON.parse(message)
+//        NSLog(json["event"].string!);
+    }
 
 }
 
